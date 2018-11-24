a791e69a4f2339aed4ecc56213c54602668eb35b
Kevin Scardina <kevin.scardina@davita.com>
2018-11-23 22:35:44 -0700

    init commit

---
+++ OTFIconFontSwift/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  dvaFontSwift
+//
+//  Created by Kevin Scardina on 11/16/18.
+//  Copyright © 2018 Kevin Scardina. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
